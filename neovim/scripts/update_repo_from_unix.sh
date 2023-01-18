df7db6a93759b5c004ba617d14d09509c25a4d01
Tim-Ole Sanmann <tim-ole.sanmann@outlook.de>
2023-01-18 07:48:57 +0100

    Added script to copy files from config folder

---
+++ neovim/scripts/update_repo_from_unix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ -z "$1" ]
+then echo "Error: Target Folder must me defined"
+    exit
+fi
+
+cd $SCRIPT_DIR
+cd ..
+cp -rv $1 .
