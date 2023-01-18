6fc67bcf243b77c0a1f335814251927c80788b45
Tim-Ole Sanmann <tim-ole.sanmann@outlook.de>
2023-01-18 08:43:01 +0100

    Improved script handling

--- neovim/scripts/update_repo_from_unix.sh
+++ neovim/scripts/update_repo_from_unix.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [ -z "$1" ]
-then echo "Error: Target Folder must me defined"
-    exit
+SOURCE=$1
+DEFAULT_SOURCE=~/.config/nvim
+
+if [ -z "$SOURCE" ] then 
+    read -p "No source defined. Use default? [$DEFAULT_SOURCE] (y/Y/n/N)" -n 1 -r
+    if [[ $REPLY =~ ˆ[Yy]$ ]]
+    then
+        SOURCE=$DEFAULT_SOURCE
+    else
+        echo "Error: Source folder must me defined"
+        exit
+    fi
 fi
 
 cd $SCRIPT_DIR
 cd ..
-cp -rv $1 .
+cp -rv $SOURCE .
--- neovim/scripts/update_unix.sh
+++ neovim/scripts/update_unix.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [ -z "$1" ]
-then echo "Error: Target Folder must me defined"
-    exit
-fi
 
+TARGET=$1
+DEFAULT_TARGET=~/.config/nvim
+
+if [ -z "$TARGET" ]
+then
+    read -p "No target defined. Use default? [$DEFAULT_TARGET] (y/Y/n/N)" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]] 
+    then
+        TARGET=$DEFAULT_TARGET
+    else
+        echo "Error: Target Folder must me defined"
+        exit
+    fi
+fi
 cd $SCRIPT_DIR
-cp -rv .. $1
+cd ..
+cp -rv ./init.vim $TARGET/init.vim
+cp -rv ./lua $TARGET
