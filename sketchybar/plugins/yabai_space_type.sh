d197124446cf6fc005c678affe0ea7e0964b605b
Tim-Ole Sanmann <tim-ole.sanmann@outlook.de>
2025-02-17 06:15:30 +0100

    Update to use aerospace

--- sketchybar/plugins/yabai_space_type.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TYPE=$(yabai -m query --spaces | jq '.[] | select(.["has-focus"] == true) | .type')
-TYPE=$(echo ${TYPE} | sed 's/"//g')
-
-
-sketchybar --set $NAME icon="" label="${TYPE}"
